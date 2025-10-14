a49df94d0c59e669c86caf5f3afccb5b04bf9ac8
JoaoNascimento1802 <joao.emanuel.pn@gmail.com>
2025-10-14 09:22:47 -0300

    Alteração do link security config

--- src/main/java/sesi/petvita/config/SecurityConfig.java
+++ src/main/java/sesi/petvita/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://vet-clinic-api-front.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://front-petvita.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
